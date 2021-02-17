f299bf0d31bd10833d7afc07a08db48220b738e9
EC2 Default User <ec2-user@ip-172-31-29-187.ec2.internal>
2021-02-17 17:21:39 +0000

    first commit

--- Arrays/DuplicateNumber.cpp
+++ Arrays/DuplicateNumber.cpp
@@ -4,3 +4,24 @@ There is only one repeated number in nums, return this repeated number.
 
 Input: nums = [1,3,4,2,2]
 Output: 2
+
+
+int findDuplicate(vector<int>& nums) { 
+       
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        
+        while(slow!=fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        
+        fast = 0;
+        while(slow!=fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
